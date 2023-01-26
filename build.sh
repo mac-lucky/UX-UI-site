216c4fba6daf5d695e388c860b61af05ae5523ea
Maciej Kędziora <maciejkedziora98@gmail.com>
2023-01-26 19:46:46 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 docker build -t maclucky/ux-ui-site .
-docker tag maclucky/ux-ui-site:latest ghcr.io/maclucky/ux-ui-site:1.0.0
-docker push ghcr.io/maclucky/ux-ui-site:1.0.0
+docker tag maclucky/ux-ui-site:latest maclucky/ux-ui-site:1.0.0
+docker push maclucky/ux-ui-site:1.0.0
